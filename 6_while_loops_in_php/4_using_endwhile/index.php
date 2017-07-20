0f243963334e702615913a7dac26b68d76eeb2a3
Craig Baird <bairdcraig@gmail.com>
2017-07-20 09:41:41 -0500

    progress

---
+++ 6_while_loops_in_php/4_using_endwhile/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>A loop of your own</title>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+    <?php
+	//Add while loop below
+    $pizzas = 0;
+    while ($pizzas < 18):
+        $pizzas ++;
+        echo "<div class=\"pizza\"></div>";
+    endwhile;
+    ?>
+    </body>
+</html>
