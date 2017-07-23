2907b5c936a9cf9aca4bb83efdbb9863df399045
Craig Baird <bairdcraig@gmail.com>
2017-07-23 17:16:37 -0500

    progress

---
+++ 6_while_loops_in_php/6_completing_the_loop/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Much a do-while</title>
+	</head>
+	<body>
+    <?php
+		$loopCond = false;
+		do {
+			echo "<p>The loop ran even though the loop condition is false.</p>";
+		} while ($loopCond);
+		
+		
+		echo "<p>Now the loop is done running.</p>";
+    ?>
+    </body>
+</html>
